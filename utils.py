b3c25c0b6ae4e9447f9ac514aa745ed87dc4b2fc
ayasameh11998 <67702469+ayasameh11998@users.noreply.github.com>
2022-05-05 21:42:50 +0200

    Add rest of utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,39 @@ operators={
 '.': lambda a:a
 } 
 
+identity = lambda x: x
+
+def shuffled(iterable):
+    """Randomly shuffle a copy of iterable."""
+    items = list(iterable)
+    shuffle(items)
+    return items
+
+def argmin_random_tie(seq, key=identity):
+    """Return a minimum element of seq; break ties at random."""
+    return min(shuffled(seq), key=key)
+
+def first(iterable, default=None):
+    """Return the first element of an iterable or the next element of a generator; or default.
+    
+    Args:
+        iterable (iterable):  The iterable to return the first element of.
+        default (object):  The default value to return if the iterable is empty.
+    Returns:
+        object: The first element of the iterable or the next element of a generator; or default.
+    """
+    try:        # try to get the first element of the iterable
+        return iterable[0]  # return the first element of the iterable
+    except IndexError:      # if the iterable is empty
+        return default      # return the default value
+    except TypeError:       # if the iterable is not an iterable
+        return next(iterable, default)  # return the next element of the generator or default
+
+
+def count(seq):
+    """Count the number of items in sequence that are interpreted as true."""
+    return sum(bool(x) for x in seq)
+
 def Adjacent_value(xy1, xy2):
     """ Return True iff the two cells are adjacent.
 
